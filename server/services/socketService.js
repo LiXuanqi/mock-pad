60f102913cd89a4a71b651466eabb2163994ba7f
lixuanqi <lixuanqi1995@gmail.com>
2018-09-01 10:30:52 -0700

    add "disconnect" and "restore changes from buffer" features.

--- server/services/socketService.js
+++ server/services/socketService.js
@@ -1,10 +1,12 @@
-// let redisClient = require("../utils/redisClient");
+let redisClient = require("../utils/redisClient");
 
 const TIMEOUT_IN_SECONDS = 3600;
 
 module.exports = function(io) {
 
   let sessionPool = [];
+
+  let sessionPath = "/temp_sessions";
 
   io.on('connection', (socket) => {
     let sessionId = socket.handshake.query['sessionId']
@@ -14,20 +16,35 @@ module.exports = function(io) {
       sessionPool[sessionId]['participants'].push(socket.id);
     } else {
       // find in redis
-
-      // there is no old data in redis, create a new session.
-      sessionPool[sessionId] = {
-        cachedChangeEvents: [],
-        participants: []
-      }
-      sessionPool[sessionId]['participants'].push(socket.id);
+      redisClient.get(`sessionPath/${sessionId}`, data => {
+        if (data) {
+          console.log("session terminated previously; pulling back from Redis");
+          sessionPool[sessionId] = {
+            cachedChangeEvents: JSON.parse(data),
+            participants: []
+          }
+        } else {
+          // there is no old data in redis, create a new session.
+          console.log("create new session")
+          sessionPool[sessionId] = {
+            cachedChangeEvents: [],
+            participants: []
+          }
+        }
+        sessionPool[sessionId]['participants'].push(socket.id);
+      });
+    
     }
 
     // handle change Event from Client
     socket.on("change", delta => {
       console.log(`change from client: ${socket.id} ${delta}`);
       if (sessionId in sessionPool) {
-        sessionPool[sessionId]['cachedChangeEvents'].push(["change", delta, Date.now()]);
+        sessionPool[sessionId]['cachedChangeEvents'].push({
+          changeName: "change",
+          data: delta,
+          time: Date.now()
+        });
       }
       forwardEvents(socket.id, sessionId, 'change', delta);
     });
@@ -40,6 +57,38 @@ module.exports = function(io) {
       cursor['socketId'] = socket.id;
 
       forwardEvents(socket.id, sessionId, 'cursorMove', JSON.stringify(cursor));
+    });
+
+    // if there is no user in the session, store the data into redis.
+    socket.on("disconnect", () => {
+      console.log(`user(${socket.id}) disconnected from ${sessionId}.`);
+      if (sessionId in sessionPool) {
+        let participants = sessionPool[sessionId]['participants'];
+        let index = participants.indexOf(socket.id);
+        if (index >= 0) {
+          participants.splice(index, 1);
+          // no user in this session
+          if (participants.length == 0) {
+            console.log("last participant left. Storing in Redis.");
+            let key = sessionPath + "/" + sessionId;
+            let value = JSON.stringify(sessionPool[sessionId]['cachedChangeEvents']);
+            redisClient.set(key, value, redisClient.redisPrint);
+            redisClient.expire(key, TIMEOUT_IN_SECONDS);
+            delete sessionPool[sessionId];
+          }
+        }
+      }
+    });
+    
+    // when user enter a existed session, restore all changes to the user.
+    socket.on("restoreBuffer", () => {
+      console.log(`restoring buffer for session: ${sessionId}, socket: ${socket.id}`);
+      if (sessionId in sessionPool) {
+        let changeEvents = sessionPool[sessionId]['cachedChangeEvents'];
+        for (let i = 0; i < changeEvents.length; i++) {
+          socket.emit(changeEvents[i]['changeName'], changeEvents[i]['data']);
+        }
+      }
     });
 
   });
